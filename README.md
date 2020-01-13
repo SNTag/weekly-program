5247aa7cb6a11d2c9902c377bd78c02db512b6af
SNTag <agenttiny@gmail.com>
2020-01-13 17:24:17 +0800

    Fully functional and basic photo-organizer

--- README.md
+++ README.md
@@ -6,11 +6,13 @@ A repository where I'll be adding programs as part of my new-years resolution: 1
 
 # P1: photo-organizer
 
-I dump all of my photos into one folder.  Using my travelling notes on when we went where in the file 'config.csv' (which is in the same folder as the photos), folders will be generated in an output location named by the city, country, and year.  symlinks to the original photos will be made inside each folder, creating a low-effort photo-organizer.
+language: python
+
+I dump all of my photos into one folder.  Using my travelling/event notes on when we went where in the file 'config.csv' (which is in the same folder as the photos), folders will be generated in an output location named by the city, country, and year.  symlinks to the original photos will be made inside each folder, creating a low-effort photo-organizer.
 
 **WARNING:** Works only on linux for the time being.
 
 ##  To Use:
 
-1. First modify the variable 'userPath' to the name of the desired output folder.  This folder has to be at path '../'.  If it does not exist, it will create one for you. 
+1. Modify (in the script) the variable 'userPath' to the name of the desired output folder.  This folder has to be at path '../'.  If it does not exist, it will create one for you.
 2. Run from the directory with the unsorted photos.
--- photo-organizer/photo-organizer.py
+++ photo-organizer/photo-organizer.py
@@ -4,6 +4,7 @@
 # Date: 28/12/2019
 # Description: To manage my photo library.  Will sort photos by country into the appropiate category.  Tested on photos from 2019 onwards
 # Usage: Simply call this program FROM THE DIRECTORY WITH UNSORTED PHOTOS.  It will read the 'config.csv' file, generate the appropiate folders under ./output/sorted (IF THEY AREN'T THERE ALREADY) and sort the photos in here into them (IF THEY AREN'T IN THERE ALREADY).  Keep note, it does not duplicate files but creates symlinks so as to reduce space consumption.
+# Version: 1.0.0
 
 
 # CSV file used to dictate this program need to have the following 1st line:
@@ -14,7 +15,6 @@
 #TODO: [C] Add a user prompt for the following line?
 userPath = "photostorage-sorted"
 
-
 #TODO: [C] make the system robust to unknown values in CSV file.
 #TODO: [C] make the system robust to files from multiple cameras (different naming conventions)
 
@@ -22,7 +22,7 @@ userPath = "photostorage-sorted"
 import pandas as pd
 import os
 import subprocess
-
+from os import walk
 
 ## Will load & format data
 fileData = pd.read_csv('./config.csv', delimiter=', ', engine="python")
@@ -31,10 +31,13 @@ fileData['Beginning'] = fileData['Beginning'].astype(int)
 fileData['Ending'] = fileData['Ending'].astype(int)
 
 ## sets up the paths
-pathOrig = os.getcwd()
-pathData = pathOrig+'/'+userPath+'/'
-if os.path.isdir(pathData) == False:
-    os.makedirs(pathData)
+pathHomeOrig = os.getcwd()
+os.chdir("../")
+pathHome = os.getcwd()
+pathSorted = pathHome+'/'+userPath+'/'
+
+if os.path.isdir(pathSorted) == False:
+    os.makedirs(pathSorted)
 
 for i in range(len(fileData["City"])):
     dataNomen = fileData.iloc[i, 2]
@@ -44,19 +47,28 @@ for i in range(len(fileData["City"])):
     strCity = fileData.loc[i,"City"]
     strCity = strCity.replace(" ","_")
     strYear = fileData.iloc[i,1]
-    fileName = strCity + '\['+ strYear + '\]'
-    fileNameFull = pathData + strCity + '['+ strYear + ']'
+    fileName = strCity + '-' + strYear
+    pathHomeFiles = pathSorted + fileName
 
     ## handles the directories
     ## makes new dir named as described in the config.csv
-    if os.path.isdir(fileNameFull) == False:
-        os.makedirs(fileNameFull)
+    if os.path.isdir(pathHomeFiles) == False:
+        os.makedirs(pathHomeFiles)
 
     ## Sorts photos by making symlinks into the appropiate folder as described by config.csv
     #TODO: [A] needs to remove all broken symlinks too!
     #TODO: [C] This approach will most likely only work on linux.  Try to make it universal?
     for d in range(dataStart, dataEnd+1):
-        something1 = pathOrig+'/input/'+dataNomen+str(d)+"*"
-        something2 = fileNameFull+'/'+dataNomen+str(d)+'.cr2'
+        something1 = pathHomeOrig+'/'+dataNomen+str(d)+"*"
+        something2 = pathHomeFiles+'/'
         bashString = str('ln -s ' + something1 + ' ' + something2)
         subprocess.call(bashString, shell = True)
+
+    ## Cleans up the directories and removes possible broken links
+    f = []
+    for (dirpath, dirnames, filenames) in walk(pathSorted):
+        f.extend(dirnames)
+        break
+    for i in f:
+        os.chdir(pathSorted+'/'+i)
+        subprocess.call(["find -L . -name . -o -type d -prune -o -type l -exec rm {} +"], shell = True)
