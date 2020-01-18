93e37150ca03ff5b873f79322c1367172177f9ae
SNTag <agenttiny@gmail.com>
2020-01-18 11:22:08 +0800

    Adding classes

--- P1-photo-organizer/photo-organizer.py
+++ P1-photo-organizer/photo-organizer.py
@@ -4,7 +4,7 @@
 # Date: 28/12/2019
 # Description: To manage my photo library.  Will sort photos by country into the appropiate category.  Tested on photos from 2019 onwards
 # Usage: Simply call this program FROM THE DIRECTORY WITH UNSORTED PHOTOS.  It will read the 'config.csv' file, generate the appropiate folders under ./output/sorted (IF THEY AREN'T THERE ALREADY) and sort the photos in here into them (IF THEY AREN'T IN THERE ALREADY).  Keep note, it does not duplicate files but creates symlinks so as to reduce space consumption.
-# Version: 1.0.1
+# Version: 1.0.2
 
 
 # CSV file used to dictate this program need to have the following 1st line:
@@ -26,66 +26,86 @@ from os import walk
 
 
 ## background processes like prepping config.csv, setting up paths, making userPath directory
-def backgroundProcess():
-    ## Will load & format data
-    dataMain = pd.read_csv('./config.csv', delimiter=', ', engine="python")
-    #TODO: [C] can I use this as a security check for a properly configured config?
-    dataMain['Beginning'] = dataMain['Beginning'].astype(int)
-    dataMain['Ending'] = dataMain['Ending'].astype(int)
-    dataMain['Ignore'] = dataMain['Ignore'].astype(bool)
+class PhotoOrganizer:
+    def __init__(self):
+        #TODO: [A] need to make all the following variables global/accesable by the function
+        ## Will load & format data
+        self.dataMain = pd.read_csv('./config.csv', delimiter=', ', engine="python")
+        #TODO: [C] can I use this as a security check for a properly configured config?
+        self.dataMain['Beginning'] = self.dataMain['Beginning'].astype(int)
+        self.dataMain['Ending'] = self.dataMain['Ending'].astype(int)
+        self.dataMain['Ignore'] = self.dataMain['Ignore'].astype(bool)
 
-    ## sets up the paths
-    pathHomeOrig = os.getcwd()
-    os.chdir("../")
-    pathHome = os.getcwd()
-    pathSorted = pathHome+'/'+userPath+'/'
+        ## sets up the paths
+        self.pathHomeOrig = os.getcwd()
+        os.chdir("../")
+        self.pathHome = os.getcwd()
+        self.pathSorted = self.pathHome+'/'+userPath+'/'
 
-    if os.path.isdir(pathSorted) == False:
-        os.makedirs(pathSorted)
+        if os.path.isdir(self.pathSorted) == False:
+            os.makedirs(self.pathSorted)
 
-## does the photo sorting/removing, making sorted folder directories
-def photoSorting():
-    backgroundProcess()
+    # #TODO: [A] need to make all the following variables global/accesable by the function
+    # ## Will load & format data
+    # dataMain = pd.read_csv('./config.csv', delimiter=', ', engine="python")
+    # #TODO: [C] can I use this as a security check for a properly configured config?
+    # self.dataMain['Beginning'] = self.dataMain['Beginning'].astype(int)
+    # self.dataMain['Ending'] = self.dataMain['Ending'].astype(int)
+    # self.dataMain['Ignore'] = self.dataMain['Ignore'].astype(bool)
 
-    for i in range(len(dataMain["City"])):
-        #TODO: [C] make this section reliant on column name, not number
-        dataNomen = dataMain.iloc[i, 2]
-        dataNomen = dataNomen.replace(" ", "")
-        dataStart = int(dataMain.iloc[i, 3])
-        dataEnd = int(dataMain.iloc[i, 4])
-        strCity = dataMain.loc[i,"City"]
-        strCity = strCity.replace(" ","_")
-        strYear = dataMain.iloc[i,1]
-        fileName = strCity + '-' + strYear
-        pathHomeFiles = pathSorted + fileName
+    # ## sets up the paths
+    # self.pathHomeOrig = os.getcwd()
+    # os.chdir("../")
+    # self.pathHome = os.getcwd()
+    # self.pathSorted = self.pathHome+'/'+userPath+'/'
 
-        ## handles the directories
-        ## makes new dir named as described in the config.csv
-        if os.path.isdir(pathHomeFiles) == False:
-            os.makedirs(pathHomeFiles)
+    # if os.path.isdir(self.pathSorted) == False:
+    #     os.makedirs(self.pathSorted)
 
-        ## Sorts/removes photos by making symlinks into the appropiate folder as described by config.csv
-        #TODO: [A] needs to remove all broken symlinks too!
-        #TODO: [C] This approach will most likely only work on linux.  Try to make it universal?
-        for d in range(dataStart, dataEnd+1):
-            something1 = pathHomeOrig+'/'+dataNomen+str(d)+"*"
-            something2 = pathHomeFiles+'/'
-            bashString = str('ln -s ' + something1 + ' ' + something2)
-            subprocess.call(bashString, shell = True)
 
-	    # ## Will remove unwanted files
-            # if dataMain["Ignore"]==True:
-            #     os.chdir(pathSorted+'/'+i)
-            #     tempString = "rm -r * " + d
-            #     subprocess.call([tempString], shell = True)
+    ## does the photo sorting/removing, making sorted folder directories
+    def photoSorting(self):
+        for i in range(len(self.dataMain["City"])):
+            #TODO: [C] make this section reliant on column name, not number
+            print(self.dataMain)
+            dataNomen = self.dataMain.iloc[i, 2]
+            dataNomen = dataNomen.replace(" ", "")
+            dataStart = int(self.dataMain.iloc[i, 3])
+            dataEnd = int(self.dataMain.iloc[i, 4])
+            strCity = self.dataMain.loc[i,"City"]
+            strCity = strCity.replace(" ","_")
+            strYear = self.dataMain.iloc[i,1]
+            fileName = strCity + '-' + strYear
+            self.pathHomeFiles = self.pathSorted + fileName
 
-        ## Cleans up the directories and removes possible broken links
-        f = []
-        for (dirpath, dirnames, filenames) in walk(pathSorted):  # Can made more efficient.
-            f.extend(dirnames)
-            break
-        for i in f:
-            os.chdir(pathSorted+'/'+i)
-            subprocess.call(["find -L . -name . -o -type d -prune -o -type l -exec rm {} +"], shell = True)
+            ## handles the directories
+            ## makes new dir named as described in the config.csv
+            if os.path.isdir(self.pathHomeFiles) == False:
+                os.makedirs(self.pathHomeFiles)
 
-photoSorting()
+            ## Sorts/removes photos by making symlinks into the appropiate folder as described by config.csv
+            #TODO: [A] needs to remove all broken symlinks too!
+            #TODO: [C] This approach will most likely only work on linux.  Try to make it universal?
+            for d in range(dataStart, dataEnd+1):
+                something1 = self.pathHomeOrig+'/'+dataNomen+str(d)+"*"
+                something2 = self.pathHomeFiles+'/'
+                bashString = str('ln -s ' + something1 + ' ' + something2)
+                subprocess.call(bashString, shell = True)
+
+                # ## Will remove unwanted files
+                # if self.dataMain["Ignore"]==True:
+                #     os.chdir(self.pathSorted+'/'+i)
+                #     tempString = "rm -r * " + d
+                #     subprocess.call([tempString], shell = True)
+
+            ## Cleans up the directories and removes possible broken links
+            f = []
+            for (dirpath, dirnames, filenames) in walk(self.pathSorted):  # Can made more efficient.
+                f.extend(dirnames)
+                break
+            for i in f:
+                os.chdir(self.pathSorted+'/'+i)
+                subprocess.call(["find -L . -name . -o -type d -prune -o -type l -exec rm {} +"], shell = True)
+
+tmp=PhotoOrganizer()
+tmp.photoSorting()
