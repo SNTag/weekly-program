1fcfaad98faa7653dd9bc18ff44ec87454149d99
SNTag <agenttiny@gmail.com>
2020-01-28 12:41:09 +0800

    moving the programs into individual repos

--- P2-sudoku/sudoku.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# File: sudoku GUI
-# Author: SNTagore (agenttiny@gmail.com)
-# Date: 14/01/2020
-# Description: part of a PyQt5 program to run sudoku.
-# Usage: This script will manage the pyqt5 gui
-
